5383d2ff9308036b901a096c9304cb8324af2bf3
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-07-18 20:46:16 +0200

    Command execution system

--- js/terminal.js
+++ js/terminal.js
@@ -37,6 +37,50 @@ function registerCommand(alias, runner, description="No description available") 
     }
 }
 
-window.addEventListener("load", () => {
+function execute(alias, args=[], flags=[]) {
+    const cmd = commands[alias]
+    if(cmd === undefined) {
+        console.error("'"+alias+"' command not found")
+        return
+    }
+    const runner = cmd.runner
+    if( typeof runner == "string") {
+        echoInnerHTML(runner)
+        return;
+    }
+    if( typeof cmd.runner == "function" ) {
+        runner(args, flags)
+        return;
+    }
+    console.error("No matching runner type")
+}
 
+function executeText(command) {
+    const split  = command.split(" ")
+    const alias = split[0]
+    let args = []
+    let flags = []
+
+    for(var i=1;i<split.length;i++){
+        const arg = split[i];
+        if(arg.startsWith("--")) flags.push(arg.replace("--"))
+        else args.push(arg)
+    }
+    execute(alias,args,flags)
+}
+
+function sendCommand(){
+    const cmd = getInput().value
+    echo(document.getElementById("prompt-pre").textContent+cmd)
+    if(cmd === "") return
+    getInput().value = ""
+    executeText(cmd)
+}
+
+window.addEventListener("load", () => {
+    getInput().addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            sendCommand()
+        }
+    })
 })
