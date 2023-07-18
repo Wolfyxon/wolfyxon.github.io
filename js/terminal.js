06cc414330c31f31f6cde90d4f97e8403b5eb87d
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-07-18 20:11:09 +0200

    Added echo functions descriptions

--- js/terminal.js
+++ js/terminal.js
@@ -10,16 +10,19 @@ function addAndGetLine() {
     getTerminal().appendChild(pre)
     return pre
 }
+// Adds a <pre> with the specified non-HTML text to the output
 function echo(text) {
     const line = addAndGetLine()
     line.textContent = text
     return line
 }
+// Adds a <pre> with the specified HTML or text to the output
 function echoInnerHTML(preHtml){
     const line = addAndGetLine();
     line.innerHTML = preHtml
     return line
 }
+// Adds an HTML code to the output
 function echoHTML(html) {
     const line = getTerminal()
     line.innerHTML += html
