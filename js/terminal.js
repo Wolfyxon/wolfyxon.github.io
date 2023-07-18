5f1b52d83ab7913df77dad85bd1d79d2821031c7
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-07-18 20:05:04 +0200

    Echo functions

--- js/terminal.js
+++ js/terminal.js
@@ -5,6 +5,22 @@ function getInput() {
     return document.getElementById("cmd-input")
 }
 
+function addAndGetLine() {
+    const pre = document.createElement("pre")
+    getTerminal().appendChild(pre)
+    return pre
+}
+function echo(text) {
+    const line = addAndGetLine()
+    line.textContent = text
+    return line
+}
+function echoHTML(html) {
+    const line = getTerminal()
+    line.innerHTML += html
+    return line
+}
+
 window.addEventListener("load", () => {
 
 })
