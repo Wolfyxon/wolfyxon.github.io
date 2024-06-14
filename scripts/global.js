a73985b74d395455db7243c7b599508c6aa5dd77
Wolfyxon <wolfyxon@gmail.com>
2024-06-14 20:28:28 +0200

    Added scroll CSS var setter

--- scripts/global.js
+++ scripts/global.js
@@ -1,3 +1,4 @@
-window.addEventListener("load", () => {
-
+window.addEventListener("scroll", () => {
+    document.body.style.setProperty("--scroll-y", window.scrollY / (document.body.offsetHeight - window.innerHeight));
+    //document.body.style.setProperty("--scroll-x", window.scrollX / (document.body.offsetWidth - window.innerWidth));
 });
