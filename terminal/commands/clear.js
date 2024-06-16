80a518bba3c0361dad83fa6f2184e8caa6450baa
Wolfyxon <wolfyxon@gmail.com>
2024-06-16 23:51:32 +0200

    Assigned description

--- terminal/commands/clear.js
+++ terminal/commands/clear.js
@@ -1,6 +1,7 @@
 registerCommand(
     new Command(["clear", "cls"])
-    
+    .setDescription("Clears the console")
+
     .setCallback(function(ctx) {
         getConsole().innerHTML = "";
     })
