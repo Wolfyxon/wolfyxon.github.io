cde98aaa637335d5e2bf3adf87b92d61981af368
Wolfyxon <wolfyxon@gmail.com>
2024-06-17 13:57:14 +0200

    Header and footer is automatically inserted

--- scripts/global.js
+++ scripts/global.js
@@ -9,8 +9,15 @@ const global = {
     })
 };
 
-window.addEventListener("load", () => {
+window.addEventListener("load", async () => {
 
+    // Auto header and footer insertion
+    const header = await utils.httpGet("/components/header.html");
+    const footer = await utils.httpGet("/components/footer.html");
+
+    document.body.innerHTML = header + document.body.innerHTML + footer;
+
+    // Observe all elements with the 'observing' class
     for(const observing of document.getElementsByClassName("observing")) {
         global.observer.observe(observing);
     }
