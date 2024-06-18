cbdad25db58fca7c604b492466e5eb8748096dfd
Wolfyxon <wolfyxon@gmail.com>
2024-06-18 15:59:30 +0200

    Init posts container and script

---
+++ blog/home.js
@@ -0,0 +1,15 @@
+const topics = [
+    "site",
+    "projects"
+];
+
+
+
+window.addEventListener("load", () => {
+    const postList = document.getElementById("posts");
+    const tmpPostListFrag = document.createDocumentFragment();
+
+
+
+    postList.append(tmpPostListFrag);
+});
