800172db3e3c046c08007e4880a335962c88db9f
Wolfyxon <wolfyxon@gmail.com>
2024-06-18 16:13:26 +0200

    Post adding logic

--- blog/home.js
+++ blog/home.js
@@ -1,4 +1,4 @@
-const topics = [
+const postTopics = [
     "site",
     "projects"
 ];
@@ -9,7 +9,43 @@ window.addEventListener("load", () => {
     const postList = document.getElementById("posts");
     const tmpPostListFrag = document.createDocumentFragment();
 
+    function registerPost(title, date, description, id, topics) {
+        const post = document.createElement("a");
+        post.href = "/" + id;
 
+        const titleEle = document.createElement("h1");
+        titleEle.innerText = title;
+        
+        const dateEle = document.createElement("span");
+        dateEle.classList.add("post-date");
+        dateEle.innerText = date;
+
+        titleEle.append(dateEle);
+        post.append(titleEle);
+
+        const textSection = document.createElement("div");
+        textSection.classList.add("post-text");
+
+        const descriptionEle = document.createElement("p");
+        descriptionEle.innerText = description;
+        
+        const topicsEle = document.createElement("div");
+        topicsEle.classList.add("post-topics");
+
+        for(const topic of topics) {
+            if(!postTopics.includes(topic)) console.warn(`Unregistered topic ${topic}`);
+
+            const ele = document.createElement("div");
+            ele.innerText = topic;
+
+            topicsEle.append(ele);
+        }
+
+        textSection.append(descriptionEle, topicsEle);
+        post.append(textSection);
+
+        tmpPostListFrag.append(post);
+    }
 
     postList.append(tmpPostListFrag);
 });
