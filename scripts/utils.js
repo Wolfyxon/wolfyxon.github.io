90ad47e7ec3407e73db0f7717d381d006cb89556
Wolfyxon <wolfyxon@gmail.com>
2024-06-20 11:54:34 +0200

    Added base64 object with encode() and decode()

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,5 +1,20 @@
 const utils = {
 
+    base64: {
+        encode: (text) => {
+            const bytes = new TextEncoder().encode(text);
+            const binStr = Array.from(bytes, (byte) => String.fromCodePoint(byte)).join("");
+
+            return btoa(binStr);
+        },
+
+        decode: (base64) => {
+            const bytes = Uint8Array.from(atob(base64), (m) => m.codePointAt(0));
+            
+            return new TextDecoder().decode(bytes);
+        }
+    },
+
     httpGet: async (url) => {
         return new Promise(((resolve, _reject) => {
             let req = new XMLHttpRequest();
