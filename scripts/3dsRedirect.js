c71dd6a618bf8f724b43a58c26c74704964cbb9c
Wolfyxon <wolfyxon@gmail.com>
2024-07-03 17:14:34 +0200

    Added redirect for Nintendo 3DS

---
+++ scripts/3dsRedirect.js
@@ -0,0 +1,5 @@
+if(navigator.userAgent.indexOf("Nintendo 3DS") !== -1) {
+    if(confirm("I see you're using a 3DS and trying to access my website which doesn't work on it. \nDid you want to go to 3DS Web Stuff?")) {
+        window.location.href = "https://wolfyxon.github.io/3ds-web-stuff";
+    }
+}
