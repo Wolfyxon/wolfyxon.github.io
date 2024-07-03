32e7f8f503df58a84b9d26393a0bbab231265668
Wolfyxon <wolfyxon@gmail.com>
2024-07-03 14:39:36 +0200

    Added soundfont registering

--- music/script.js
+++ music/script.js
@@ -1,3 +1,19 @@
 window.addEventListener("load", () => {
+    const soundfontList = document.getElementById("soundfont-list");
+    const soundFontListFrag = document.createDocumentFragment();
 
+    function registerSoundfont(name, url) {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+
+        a.innerText = name;
+        a.href = url;
+
+        li.append(a);
+        soundFontListFrag.append(li);
+    }
+
+    registerSoundfont("FluidR3 GM", "https://member.keymusician.com/Member/FluidR3_GM/");
+
+    soundfontList.append(soundFontListFrag);
 });
