9ad90b23e0dcf68e8a50a329399237244ac754e5
Wolfyxon <wolfyxon@gmail.com>
2024-07-04 10:34:50 +0200

    Added soundfont author field

--- music/script.js
+++ music/script.js
@@ -2,21 +2,24 @@ window.addEventListener("load", () => {
     const soundfontList = document.getElementById("soundfont-list");
     const soundFontListFrag = document.createDocumentFragment();
 
-    function registerSoundfont(name, url) {
+    function registerSoundfont(name, author, url) {
         const li = document.createElement("li");
         const a = document.createElement("a");
+        const authorSpan = document.createElement("span");
 
         a.innerText = name;
         a.href = url;
 
-        li.append(a);
+        authorSpan.innerText = " by " + author;
+
+        li.append(a, authorSpan);
         soundFontListFrag.append(li);
     }
 
-    registerSoundfont("FluidR3 GM", "https://member.keymusician.com/Member/FluidR3_GM/");
-    registerSoundfont("Organteq 2 Church Organ", "https://musical-artifacts.com/artifacts/3579");
-    registerSoundfont("General User GS", "https://schristiancollins.com/generaluser.php");
-    registerSoundfont("Amen Break", "https://musical-artifacts.com/artifacts/974");
+    registerSoundfont("FluidR3 GM", "", "https://member.keymusician.com/Member/FluidR3_GM/");
+    registerSoundfont("Organteq 2 Church Organ", "", "https://musical-artifacts.com/artifacts/3579");
+    registerSoundfont("General User GS", "", "https://schristiancollins.com/generaluser.php");
+    registerSoundfont("Amen Break", "", "https://musical-artifacts.com/artifacts/974");
     
 
     soundfontList.append(soundFontListFrag);
