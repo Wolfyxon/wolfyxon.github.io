66263ea48a8652f5e9003bc46eaa0bce21795e33
Wolfyxon <wolfyxon@gmail.com>
2024-07-04 10:40:02 +0200

    Assigned all authors

--- music/script.js
+++ music/script.js
@@ -16,10 +16,10 @@ window.addEventListener("load", () => {
         soundFontListFrag.append(li);
     }
 
-    registerSoundfont("FluidR3 GM", "", "https://member.keymusician.com/Member/FluidR3_GM/");
-    registerSoundfont("Organteq 2 Church Organ", "", "https://musical-artifacts.com/artifacts/3579");
-    registerSoundfont("General User GS", "", "https://schristiancollins.com/generaluser.php");
-    registerSoundfont("Amen Break", "", "https://musical-artifacts.com/artifacts/974");
+    registerSoundfont("FluidR3 GM", "Frank Wen & contributors", "https://member.keymusician.com/Member/FluidR3_GM/");
+    registerSoundfont("Organteq 2 Church Organ", "Simone Piervergili", "https://musical-artifacts.com/artifacts/3579");
+    registerSoundfont("General User GS", "S. Christian Collins", "https://schristiancollins.com/generaluser.php");
+    registerSoundfont("Amen Break", "ASmolBoy / VEXST", "https://musical-artifacts.com/artifacts/974");
     
 
     soundfontList.append(soundFontListFrag);
