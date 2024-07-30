75023735ef53d8f39a26d4488ab972e39fad2996
Wolfyxon <wolfyxon@gmail.com>
2024-07-30 16:38:22 +0200

    Added a navbar for navigating through headers

--- blog/post.js
+++ blog/post.js
@@ -1,4 +1,8 @@
 window.addEventListener("load", () => {
+    const main = document.getElementById("main");
+
+    // -= Static components =-
+
     const hd = document.createElement("div");
     hd.classList.add("post-header");
 
@@ -10,7 +14,76 @@ window.addEventListener("load", () => {
     date.innerText = document.body.getAttribute("data-post-date");
     date.classList.add("post-date");
 
-    hd.append(title, date);
+    // -= Nav =-
+    const nav = document.createElement("div"); 
+    nav.id = "post-nav";
+    
+    const navRoot = document.createElement("ul");
+    navRoot.id = "post-nav-root";
+
+    const navBtn = document.createElement("button");
+    navBtn.id = "post-nav-btn";
+    let navOpen = false;
+
+    navBtn.addEventListener("click", () => {
+        if(navOpen) closeNav();
+        else openNav();
+    });
+
+    function openNav() {
+        navOpen = true;
+        navRoot.style.maxHeight = "100px";
+        navRoot.style.opacity = "1";
+    }
+
+    function closeNav() {
+        navOpen = false;
+        navRoot.style.maxHeight = "0px";
+        navRoot.style.opacity = "0";
+    }
+
+    closeNav();
+   
+    let currentList = navRoot;
+    let currentLevel = 1;
+
+    utils.getDescendants(main).forEach(elm => {
+        if(elm.tagName.startsWith("H") && elm.tagName.length === 2) {
+            const level = parseInt(elm.tagName.replace("H", ""));
+
+            if(level < currentLevel) {
+                for(let i = 0; i < level; i++) {
+                    currentList = currentList.parentElement;
+                }
+            } else if(level > currentLevel) {
+                const list = document.createElement("ul");
+                currentList.append(list);
+                currentList = list;
+            }
+
+            currentLevel = level;
+
+            const link = document.createElement("li");
+            link.innerText = elm.innerText;
+            
+            link.addEventListener("click", () => {
+                if(navRoot.style.opacity === "0") return;
+
+                elm.scrollIntoView({
+                    "behavior": "smooth"
+                });
+            });
+
+            currentList.append(link);
+        }
+    });
+    
+    
+    nav.append(navBtn, navRoot);
+
+    // -= Finish the header =-
+
+    hd.append(title, date, nav);
     document.body.append(hd);
     document.body.insertBefore(hd, document.getElementById("main"));
 });
