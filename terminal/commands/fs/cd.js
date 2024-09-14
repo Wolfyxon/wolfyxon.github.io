108b267080702d6f620e483bb9eebb8b4f285227
Wolfyxon <wolfyxon@gmail.com>
2024-09-14 12:23:55 +0200

    Added 'cd'

---
+++ terminal/commands/fs/cd.js
@@ -0,0 +1,26 @@
+registerCommand(
+    new Command(["cd"])
+    .setDescription("Changes the working directory")
+    .addArg("path", "Directory path", false)
+    
+    .setCallback(async (ctx) => {
+        const path = ctx.getArg("path") || "/";
+        const res = await queryFs(path);
+
+        if(!res) {
+            echo(`cd: ${path}: No such file or directory`);
+            return;
+        }
+
+        if(typeof(res) == "string") {
+            echo(`bash: cd: ${path}: Not a directory`);
+            return;
+        }
+
+        if(path.startsWith("/")) {
+            currentDir = path;
+        } else {
+            currentDir = currentDir + path;
+        }
+    })
+);
