c1862e12ca5432ccfd095be2f9493bb3527eb0fa
Wolfyxon <wolfyxon@gmail.com>
2025-01-22 14:47:31 +0100

    Init Project component

---
+++ src/pages/home/Project.tsx
@@ -0,0 +1,16 @@
+function Project(data: {title: string, description: string, thumbnail: string, children: any}) {
+    return (
+        <div>
+            <div className="project-text">
+                <h2>{data.title}</h2>
+                <p>{data.description}</p>
+
+                {data.children}
+            </div>
+
+            <img src={data.thumbnail} alt="thumbnail" />
+        </div>
+    )
+}
+
+export default Project
