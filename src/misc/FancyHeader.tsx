fa72021d8edd39fe3ae986aa1c79a0a75292207f
Wolfyxon <wolfyxon@gmail.com>
2025-01-22 13:04:15 +0100

    Added FancyHeader

---
+++ src/misc/FancyHeader.tsx
@@ -0,0 +1,11 @@
+function FancyHeader(data: {children: any}) {
+    return (
+        <h1 className="fancy-h">
+            <div>
+                {data.children}
+            </div>
+        </h1>
+    )
+}
+
+export default FancyHeader
