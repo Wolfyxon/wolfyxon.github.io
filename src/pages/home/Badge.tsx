4f43c1f4a64b7031bd954a28e9f5928e7c244552
Wolfyxon <wolfyxon@gmail.com>
2025-01-22 12:49:01 +0100

    Proper params

--- src/pages/home/Badge.tsx
+++ src/pages/home/Badge.tsx
@@ -1,6 +1,6 @@
-function Badge(icon: string, alt: string) {
+function Badge(data: {icon: string, alt: string}) {
     return (
-        <img className="badge" src={"/assets/img/icons/" + icon} alt={alt} />
+        <img className="badge" src={"/assets/img/icons/" + data.icon} alt={data.alt} />
     )
 }
 
