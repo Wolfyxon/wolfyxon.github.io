cb5134869e8ecd13e6e092bcfaba979b57095c3e
Wolfyxon <wolfyxon@gmail.com>
2025-01-22 18:07:11 +0100

    Fixed scripts and readded commands

--- src/pages/terminal/Terminal.tsx
+++ src/pages/terminal/Terminal.tsx
@@ -1,9 +1,37 @@
-import "../../js/utils.js"
-import "./script.js"
 import "./style.css"
 import "../../css/animations.css"
 
+import { useEffect } from "react"
+
 function Terminal() {
+
+    // TODO: Fix scripts adding multiple times for some reason
+    useEffect(() => {
+        function addScript(src: string) {
+            const script = document.createElement("script");
+
+            script.src = "/js/terminal/" + src;
+            document.body.appendChild(script);
+
+            return () => {
+                document.body.removeChild(script);
+            }
+        }
+
+        addScript("script.js");
+
+        addScript("commands/help.js");
+        addScript("commands/echo.js");
+        addScript("commands/clear.js");
+        addScript("commands/exit.js");
+        addScript("commands/reboot.js");
+        addScript("commands/dropbox-download.js");
+        addScript("commands/badapple.js");
+        addScript("commands/browserfetch.js");
+        addScript("commands/base64-encode.js");
+        addScript("commands/base64-decode.js");
+    })
+
     return (
         <div id="main">
             <pre id="console"></pre>
