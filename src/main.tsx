2d800f0ee0023edd2fcdbf4e4f16058df33fd8fd
Wolfyxon <wolfyxon@gmail.com>
2025-01-22 16:19:13 +0100

    Multi page support

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
 
 import './css/colors.css'
 import './css/global.css'
 
 import Home from './pages/home/Home.tsx'
+
 import Header from './global/Header.tsx'
 import Footer from './global/Footer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Header />
-    
+
     <div id="main">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={Home} />
+        </Routes>
+      </BrowserRouter>
     </div>
 
     <Footer />
