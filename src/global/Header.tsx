83d362b41834fb4e058c57ce69a11d2d962098ef
Wolfyxon <wolfyxon@gmail.com>
2025-01-22 12:30:17 +0100

    Implemented the header

--- src/global/Header.tsx
+++ src/global/Header.tsx
@@ -3,7 +3,29 @@ import "./Header.css"
 function Header() {
     return (    
         <header>
-            hi
+            <div id="header-title">
+                <div>
+                        <a href="/" id="header-title-home-link">Wolfyxon</a>
+                        
+                        <a id="header-page-title" style={{display: "none"}}>
+                            <span></span>
+                        </a>
+                </div>
+            </div>
+            <nav>
+                <a href="/blog" id="header-link-blog">
+                    <div>Blog</div>
+                </a>
+                <a href="/about" id="header-link-about">
+                    <div>About</div>
+                </a>
+                <a href="/goals" id="header-link-goals">
+                    <div>Goals</div>
+                </a>
+                <a href="/terminal" id="header-link-terminal">
+                    <div>Terminal</div>
+                </a>
+            </nav>
         </header>
     )
 }
