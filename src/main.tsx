2b9b11ac311862df73e01df79479d013bb933952
Wolfyxon <wolfyxon@gmail.com>
2025-01-23 16:24:04 +0100

    Better routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,12 @@ import { StrictMode, lazy } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 
+import { pages } from "./siteMap.tsx" 
+
 import './css/colors.css'
 import './css/global.css'
 
-const Home = lazy(() => import('./pages/home/Home.tsx'))
 const Redirect = lazy(() => import('./Redirect.tsx'))
-const Blog = lazy(() => import('./pages/blog/Blog.tsx'))
-const About = lazy(() => import('./pages/about/About.tsx'))
-const Goals = lazy(() => import('./pages/goals/Goals.tsx'))
 
 import Page from './Page.tsx'
 import BlogPost from './pages/blog/BlogPost.tsx'
@@ -18,28 +16,21 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <BrowserRouter>
         <Routes>
-          {["/", "/index.html"].map((v) => {
-              return <Route path={v} element={
-              <Page title="Home" home="/"><Home /></Page>
-            } />
-          })}
-          
-          <Route path="/blog" element={
-            <Page title="Blog" home="/blog"><Blog /></Page>
-          } />
+          {
+            Object.entries(pages).map((entry) => {              
+              const path = entry[0];
+              const page = entry[1];
 
-        <Route path="/blog/*" element={
-            <Page title="Blog" home="/blog"><BlogPost /></Page>
-          } />
-
-
-          <Route path="/about" element={
-            <Page title="About" home="/about"><About /></Page>
-          } />
-
-          <Route path="/goals" element={
-            <Page title="Goals" home="/goals">< Goals/></Page>
-          } />
+              const pageElm = <Page title={page.title} home=''> {page.element} </Page>
+              
+              return (
+                <>
+                  <Route path={path} element={pageElm}/>
+                  <Route path={path + "/index.html"} element={pageElm}/>
+                </>
+              );
+            })
+          }
 
           <Route path="/terminal" element={<Redirect to="index.html" />} />
         </Routes>
