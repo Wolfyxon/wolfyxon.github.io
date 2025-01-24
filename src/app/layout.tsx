80cef01945b870c75412272a27d2a75b6ce56cdd
Wolfyxon <wolfyxon@gmail.com>
2025-01-24 19:31:29 +0100

    Removed new line

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,6 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>) {
-
   return (
     <html lang="en">
       <head>
