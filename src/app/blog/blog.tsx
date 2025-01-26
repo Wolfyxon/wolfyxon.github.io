4bfa5765d01a8e8477549a826958923a06e37395
Wolfyxon <wolfyxon@gmail.com>
2025-01-26 11:24:03 +0100

    Fixed slug

--- src/app/blog/blog.tsx
+++ src/app/blog/blog.tsx
@@ -1,5 +1,6 @@
 import * as fs from "fs";
-import matter, { GrayMatterFile } from "gray-matter";
+import matter from "gray-matter";
+import { basename } from "path";
 
 export type PostData = {
     title: string,
@@ -18,13 +19,15 @@ export async function getPostFileNames(): Promise<string[]> {
 export async function parsePost(path: string): Promise<PostData> {
     const text = await fs.readFileSync(path);
     const mat = matter(text);
+    
+    const slug = basename(path.replace(postDir, "").replace(".md", ""));
 
     return {
         title: mat.data.title,
         description: mat.data.description,
         date: new Date(mat.data.date),
         markdown: mat.content,
-        slug: path.replace(".md", ""),
+        slug: slug,
     };
 }
 
