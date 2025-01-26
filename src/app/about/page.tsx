47cb4db448779db296bc7dbfa7e5b0862b0c561f
Wolfyxon <wolfyxon@gmail.com>
2025-01-26 15:49:47 +0100

    Added about me page content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,9 +11,49 @@ export const metadata: Metadata = {
 export default function About() {
     return (
         <>
-            <section>
-                <h1>Hi</h1>
-            </section>
+            <div className="fancy-section">
+                <section>
+                    <h1>Who am I</h1>
+                    <p>
+                        Hi, I'm Wolfyxon.
+                    </p>
+                    <p>
+                        I'm a young programmer from Poland. <br />
+                        I've been coding since I was 9 years old and it truly is my passion. 
+                    </p>
+                    <p>
+                        The stuff I primarily make is tools, websites and games. 
+                        I code for fun and enjoy learning new technologies.
+                    </p>
+                    <p>
+                        I'm also autistic btw.
+                    </p>
+                </section>
+
+                <section>
+                    <h1>Interests</h1>
+                    <p>
+                        Programming is my primary interest (a passion, actually) but I'm also into
+                        composing music, video and audio editing, science and psychology.
+                    </p>
+                    <p>
+                        I enjoy dealing with all kinds of software, especially if it provides a lot of control.
+                        That's why I use Linux and a lot of open source tools. 
+                    </p>
+                </section>
+
+                <section>
+                    <h1>How I code</h1>
+                    <p>
+                        I'm definitely on the craft-based side of programmers.
+                    </p>
+                    <p>
+                        I care a lot about code maintainability, structure, style and proper practices.
+                        I would call myself a perfectionist in that <br/>
+                        (expect me to have a mental breakdown when looking at your code).
+                    </p>
+                </section>
+            </div>
         </>
     );
 }
