459a85721f6deae11496fae4a8429da2f1bbaf54
Wolfyxon <wolfyxon@gmail.com>
2025-01-28 15:51:01 +0100

    Reorganized

--- src/templates/Page.tsx
+++ src/templates/Page.tsx
@@ -1,7 +1,7 @@
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 
-export default function Main(data: { children: any, category: string, homeUrl?: string }) {
+export default function Page(data: { children: any, category: string, homeUrl?: string }) {
     return (
         <>
             <Header title={data.category} home={data.homeUrl} />
