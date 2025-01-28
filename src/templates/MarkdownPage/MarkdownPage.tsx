d0015a261bd5503ffaf62241ce24930c9db3a7cb
Wolfyxon <wolfyxon@gmail.com>
2025-01-28 22:25:24 +0100

    Support for normal elements

--- src/templates/MarkdownPage/MarkdownPage.tsx
+++ src/templates/MarkdownPage/MarkdownPage.tsx
@@ -1,23 +1,31 @@
 import * as fs from "fs";
 import Markdown from "markdown-to-jsx";
+import { ReactNode } from "react";
 
 import "@/app/css/animations.css";
 import "./MarkdownPage.css";
 
-export default async function MarkdownPage(data: {children: string, src?: string}) {
-    let markdown = "";
+export default async function MarkdownPage(data: {children: string | ReactNode | ReactNode[], src?: string}) {
 
-    if(data.src) {
-        markdown = await fs.readFileSync(data.src).toString();
+    let content: ReactNode = null;
+
+    if(typeof(data.children) == "string" || data.src) {
+        let markdown = "";
+
+        if(data.src) {
+            markdown = await fs.readFileSync(data.src).toString();
+        } else {
+            markdown = data.children as string;
+        }
+        
+        content = <Markdown>{markdown}</Markdown>
     } else {
-        markdown = data.children;
+        content = data.children;
     }
-    
+
     return (
         <article className="article-page">
-            <Markdown>
-                {markdown}
-            </Markdown>
+            {content}
         </article>
     )
 }
