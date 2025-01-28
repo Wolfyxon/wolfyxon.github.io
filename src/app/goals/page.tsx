e28123fa0c47de031248acf34351cdaecb57ce6f
Wolfyxon <wolfyxon@gmail.com>
2025-01-28 22:53:34 +0100

    Updated goals

--- src/app/goals/page.tsx
+++ src/app/goals/page.tsx
@@ -28,10 +28,12 @@ export default function Goals() {
             <h2>In progress</h2>
             <GoalList id="goals-in-progress">
                 <Goal title="Learn music" progress={80}></Goal>
-                <Goal title="Learn art" progress={43}></Goal>
+                <Goal title="Learn art" progress={30}></Goal>
                 <Goal title="Learn cybersecurity" progress={10}></Goal>
+                <Goal title="Learn electricity & electronics" progress={5}></Goal>
                 <Goal title="Make a programming language" progress={2}></Goal>
                 <Goal title="Heal form social anxiety" progress={40}></Goal>
+                <Goal title="Write a story" progress={0}></Goal>
                 <Goal title="Save someone's life" progress={0}></Goal>
             </GoalList>
 
