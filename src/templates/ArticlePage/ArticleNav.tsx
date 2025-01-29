8154d0315e04f663f4270e756ff10d1edaa9e999
Wolfyxon <wolfyxon@gmail.com>
2025-01-29 12:50:54 +0100

    Added nesting

--- src/templates/ArticlePage/ArticleNav.tsx
+++ src/templates/ArticlePage/ArticleNav.tsx
@@ -6,15 +6,33 @@ export default function ArticleNav() {
     const ref = useRef(null);
     
 
-    useEffect(() => { // TODO: Nesting
+    useEffect(() => {
         const nav = ref.current! as HTMLElement;
         
         const headings = document.querySelectorAll(".article-page *:is(h1, h2, h3, h4, h5, h6)");
         
-        let rootList = document.createElement("ul");
+        const rootList = document.createElement("ul");
+        
+        let current = rootList;
+        let currentDepth = 1;
 
         for(const h of headings) {
-            rootList.innerHTML += `<li><a href="#${h.id}">${h.innerHTML}</a></li>`;
+            const depth = parseInt(h.tagName.replace("H", ""));
+
+            if(depth != currentDepth) {
+                if(depth > currentDepth) {
+                    const list = document.createElement("ul");
+
+                    current.appendChild(list);
+                    current = list;
+                } else {
+                    current = current.parentElement! as HTMLUListElement;
+                }
+
+                currentDepth = depth;
+            }
+
+            current.innerHTML += `<li><a href="#${h.id}">${h.innerHTML}</a></li>`;
         }
 
         nav.appendChild(rootList);
