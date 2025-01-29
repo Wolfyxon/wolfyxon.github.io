0da545ef7f1e60263ec532edc832ef47dd1ea7bd
Wolfyxon <wolfyxon@gmail.com>
2025-01-29 20:48:59 +0100

    Added post date on the bottom

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -4,6 +4,8 @@ import { getBySlug, getPosts } from "../blog";
 import Page from "@/templates/Page";
 import ArticlePage from "@/templates/ArticlePage/ArticlePage";
 
+import "./blogPost.css";
+
 export async function generateStaticParams() {
     const posts = await getPosts();
     return posts.map((v) => { return { slug: v.slug }});
@@ -28,6 +30,8 @@ export default async function BlogPost(data: {params: any}) {
             <ArticlePage>
                 {post.markdown}
             </ArticlePage>
+            
+            <p className="blog-post-date">{post.date.toDateString()}</p>
         </Page>
     );
 }
