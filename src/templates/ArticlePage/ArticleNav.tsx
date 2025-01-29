ddf3274560b517c3d489ec5290190a0c09b23474
Wolfyxon <wolfyxon@gmail.com>
2025-01-29 10:23:47 +0100

    Adding headings to nav

--- src/templates/ArticlePage/ArticleNav.tsx
+++ src/templates/ArticlePage/ArticleNav.tsx
@@ -1,16 +1,27 @@
 "use client"
 
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 
 export default function ArticleNav() {
-    useEffect(() => {
-        const headings = document.querySelectorAll(".article-page *:is(h1, h2, h3, h4, h5, h6)");
+    const ref = useRef(null);
+    
 
+    useEffect(() => { // TODO: Nesting
+        const nav = ref.current! as HTMLElement;
         
+        const headings = document.querySelectorAll(".article-page *:is(h1, h2, h3, h4, h5, h6)");
+        
+        let rootList = document.createElement("ul");
+
+        for(const h of headings) {
+            rootList.innerHTML += `<li><a href="#${h.id}">${h.innerHTML}</a></li>`;
+        }
+
+        nav.appendChild(rootList);
     });
 
     return (
-        <nav className="article-nav">
+        <nav className="article-nav" ref={ref}>
             <h1>On this page</h1>
 
         </nav>
