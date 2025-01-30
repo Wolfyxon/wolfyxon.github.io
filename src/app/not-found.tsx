6c028f0b805a19dc80040004cf758a10c288a9fa
Wolfyxon <wolfyxon@gmail.com>
2025-01-30 17:46:42 +0100

    Added 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import Redirect from "@/components/Redirect";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Page not found",
+    description: "This page does not exist I guess"
+}
+
+export default function NotFound() {
+    return (
+        <>
+            <h1>404</h1>
+            <p>Page not found, prepare thyself</p>
+
+            <Redirect url="https://www.youtube.com/watch?v=dQw4w9WgXcQ" />
+        </>
+    )
+}
