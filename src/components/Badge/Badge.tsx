9a80619c7baa66bdc3d82210c997b25397e8320c
Wolfyxon <wolfyxon@gmail.com>
2025-02-18 13:51:42 +0100

    Hover badge text

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,5 +1,10 @@
+import "./Badge.css";
+
 export default function Badge(data: {icon: string, alt: string}) {
     return (
-        <img className="badge-img" src={"/assets/media/img/icons/" + data.icon} alt={data.alt} />
+        <div className="badge">
+            <img className="badge-img" src={"/assets/media/img/icons/" + data.icon} alt={data.alt} />
+            <div className="badge-label">{data.alt}</div>
+        </div>
     )
 }
