e4b91c619a1069dd2c1d8b7b491dfdd7d4f94f86
Wolfyxon <wolfyxon@gmail.com>
2025-03-01 23:48:33 +0100

    Init music page

---
+++ src/app/music/page.tsx
@@ -0,0 +1,17 @@
+import Page from "@/layouts/Page";
+
+import "./music.css";
+import LargeBadge from "@/components/LargeBadge/LargeBadge";
+
+export default function MusicPage() {
+    return (
+        <Page category="Music">
+            <h1>Listen to my music on:</h1>
+            
+            <div id="links">
+                <LargeBadge title="YouTube" icon="/assets/media/img/icons/social/youtube.png" url="https://www.youtube.com/playlist?list=PLtM1NLG1o7IidEvwmwbUCxkJnukupLA0Z" />
+                <LargeBadge title="SoundCloud" icon="/assets/media/img/icons/social/soundcloud.png" url="https://soundcloud.com/wolfyxon" />
+            </div>
+        </Page>
+    )
+}
