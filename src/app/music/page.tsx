8ee3742a484748ba0b126d67e04eccf9f2b0bb50
Wolfyxon <wolfyxon@gmail.com>
2025-03-02 13:22:27 +0100

    lmms

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -12,6 +12,13 @@ export default function MusicPage() {
                 <LargeBadge title="YouTube" icon="/assets/media/img/icons/social/youtube.png" url="https://www.youtube.com/playlist?list=PLtM1NLG1o7IidEvwmwbUCxkJnukupLA0Z" />
                 <LargeBadge title="SoundCloud" icon="/assets/media/img/icons/social/soundcloud.png" url="https://soundcloud.com/wolfyxon" />
             </div>
+
+            <h1>What I use to make music:</h1>
+
+            <div>
+                <LargeBadge title="LMMS" icon="/assets/media/img/icons/software/tools/lmms.png" url="https://lmms.io" />
+            </div>
+            
             <p>Page under construction eh</p>
         </Page>
     )
