45b2d0188408b7f14ea7e037b54b7db1074ed5c2
Wolfyxon <wolfyxon@gmail.com>
2025-03-02 13:23:17 +0100

    Turned #links into .cards

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -8,17 +8,17 @@ export default function MusicPage() {
         <Page category="Music">
             <h1>Listen to my music on:</h1>
             
-            <div id="links">
+            <div className="cards">
                 <LargeBadge title="YouTube" icon="/assets/media/img/icons/social/youtube.png" url="https://www.youtube.com/playlist?list=PLtM1NLG1o7IidEvwmwbUCxkJnukupLA0Z" />
                 <LargeBadge title="SoundCloud" icon="/assets/media/img/icons/social/soundcloud.png" url="https://soundcloud.com/wolfyxon" />
             </div>
 
             <h1>What I use to make music:</h1>
 
-            <div>
+            <div className="cards">
                 <LargeBadge title="LMMS" icon="/assets/media/img/icons/software/tools/lmms.png" url="https://lmms.io" />
             </div>
-            
+
             <p>Page under construction eh</p>
         </Page>
     )
