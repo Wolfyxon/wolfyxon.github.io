5be9c02ab594d06d91729d75f42ad139f8969098
Wolfyxon <wolfyxon@gmail.com>
2025-03-02 21:38:04 +0100

    Init logic

--- src/app/newTerminal/page.tsx
+++ src/app/newTerminal/page.tsx
@@ -1,6 +1,51 @@
+"use client";
+
+import { useEffect } from "react";
+import { getConsole, getInput } from "./terminal";
+import { CommandRunContext } from "./Command";
+
 import "./terminal.css";
 
 export default function TerminalPage() {
+    useEffect(() => {
+        const input = getInput();
+        const console = getConsole();
+
+        let currentCtx: CommandRunContext | undefined;
+
+        function sendText(text: string) {
+            if(text === "^C") {
+                sigTerm();
+                return;
+            }
+
+            if(!currentCtx) return;
+        }
+
+        function execute() {
+            
+        }
+
+        function sigTerm() {
+            // TODO
+        }
+
+        window.addEventListener("keydown", (e) => {
+            if(document.activeElement !== input) return;
+    
+            if(e.key == "Enter") {
+                const txt = input.value;
+                input.value = "";
+                sendText(txt);
+            }
+    
+            if(e.ctrlKey && e.key === "c") {
+                getInput().value += "^C";
+                sigTerm();
+            }
+        });
+    }, []);
+
     return (
         <>
             <pre id="console"></pre>
