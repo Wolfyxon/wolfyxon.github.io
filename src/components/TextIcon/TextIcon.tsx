b515a5ad05b2fbbd736c45a23a76353264888bad
Wolfyxon <wolfyxon@gmail.com>
2025-03-20 20:56:42 +0100

    Added TextIcon component

---
+++ src/components/TextIcon/TextIcon.tsx
@@ -0,0 +1,7 @@
+import "./TextIcon.css";
+
+export default function TextIcon(data: {src: string, alt: string}) {
+    return (
+        <img src={data.src} alt={data.alt} className="text-icon" />
+    );
+}
