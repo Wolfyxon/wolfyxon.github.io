35245704c9171634506b0e43dba26d0af56933f4
Wolfyxon <wolfyxon@gmail.com>
2025-04-02 14:45:50 +0200

    Added USSR

---
+++ src/app/ose/ussr/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+import OsePage from "../OsePage";
+import TextLink from "@/components/TextLink";
+
+export const metadata: Metadata = {
+    title: "Ogólnoruska Sieć Radziecka"
+}
+
+export default function UssrOsePage() {
+    return <OsePage 
+            icon="/assets/media/img/ose/ussr.png" 
+            title={metadata.title as string}
+            heading="Ta strona zawiera kapitalizm"
+            category="Capitalism"
+            dynamicCategory="CAPITALISM USA NATO POLAND">
+
+            <p>
+                Dostęp do tej strony został zablokowany ze względu na usługę Ochrona Obywatela ZSRR, która jest włączona w NASZYM związku.
+            </p>
+
+            <p>Więcej o usłudze na <TextLink>https://en.wikipedia.org/wiki/Soviet_Union</TextLink>.</p>
+
+        </OsePage>
+}
