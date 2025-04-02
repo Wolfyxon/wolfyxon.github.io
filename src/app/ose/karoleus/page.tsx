28767971b88a635f695ea1ab890f5db4d4769072
Wolfyxon <wolfyxon@gmail.com>
2025-04-02 21:31:20 +0200

    Added karoleus

---
+++ src/app/ose/karoleus/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+import OsePage from "../OsePage";
+import TextLink from "@/components/TextLink";
+
+export const metadata: Metadata = {
+    title: "Ogólnorybska Sieć Szynki"
+}
+
+export default function LinusOsePage() {
+    return <OsePage 
+            icon="/assets/media/img/people/karoleus_pl.png" 
+            title={metadata.title as string}
+            heading="Ta strona zawiera klasy"
+            category="Wolfyxon's code"
+            dynamicCategory="WOLFYXONS_CODE OBJECT_ORIENTED_PROGRAMMING CLASSES">
+
+            <p>
+                Dostęp do tej strony został zablokowany ze względu na usługę Ochrona Programisty Karoleus, która jest włączona w Twoim kodzie.
+            </p>
+
+            <p>Więcej o usłudze na <TextLink>https://karoleuspl.github.io</TextLink>.</p>
+
+        </OsePage>
+}
