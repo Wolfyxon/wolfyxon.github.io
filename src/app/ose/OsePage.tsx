4f3ac29e1bedf11c7d205920ebd9d5555c7e72f4
Wolfyxon <wolfyxon@gmail.com>
2025-04-02 12:47:52 +0200

    Init OSE Page template

---
+++ src/app/ose/OsePage.tsx
@@ -0,0 +1,61 @@
+import { Metadata } from "next";
+
+import "./OsePage.css";
+import { Content } from "@/utils";
+
+export const metadata: Metadata = {
+    title: {
+        template: "%s",
+        default: "Odmowa dostępu"
+    }
+}
+
+export type OsePageProps = {
+    title: string, 
+    icon: string, 
+    heading: string,
+    category: string,
+    dynamicCategory: string,
+    children?: Content
+}
+
+export default function OsePage(data: OsePageProps) {
+    return (
+    <>
+        <p className="center">
+            <a href="https://plug.ose.gov.pl/">
+                <img src={data.icon} alt="-S" />
+            </a>
+        </p>
+
+        <p className="l-text center">{data.heading} ☹</p>
+
+        <div id="b-texts">
+            {data.children}
+        </div>
+
+        <hr />
+
+        <p>Wygenerowano na wolf841x.ose.net.pl , 2025/02/12 12:27:04 dla adresu 4096:1b3:69420:a7be:2137:17ff:cd02:ff55</p>
+        <p>
+            <span className="msg">Zablokowano przez silnik analizy URL</span>
+        </p>
+
+        <p>Żądanie zostało zablokowane przez silnik analizy URL. Strona WWW należy do kategorii blokowanych przez system.</p>
+        <p><strong>URL:</strong> https://wolfyxon.github.io</p>
+        <p><strong>Kategoria URL:</strong> {data.category}</p>
+
+        <p id="cat" style={{"display": "block"}}>
+            <strong>Kategoria z Dynamicznej Analizy Treści:</strong> 
+            <span id="cat-val">{data.dynamicCategory}</span>
+        </p>
+
+        <p id="id" style={{"display": "block"}}>
+            <strong>ID: </strong>
+            <span id="id-val">02</span>
+        </p>
+        
+        <hr />
+    </>
+    );
+}
---
+++ src/app/ose/pis/page.tsx
@@ -0,0 +1,18 @@
+import OsePage from "../OsePage";
+
+export default function PisOsePage() {
+    return <OsePage 
+            icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUn8e5hpB_Kmvd_af_5XBR-3hT5DLF4qp6qw&ss" 
+            title="Ogólnopolska Sieć Antylewacyjna"
+            heading="Ta strona zawiera lewactwo"
+            category="Lewactwo"
+            dynamicCategory="LEWACTWO BRAINROT SKIBIDI_TOILET">
+
+            <p>
+                Dostęp do tej strony został zablokowany ze względu na usługę Ochrona Obywatela RP, która jest włączona w Twoim państwie.
+            </p>
+
+            <p>Chwała PiS! Niech żyje Kaczyński! <a href="https://pis.org.pl/">https://pis.org.pl/</a>.</p>
+
+        </OsePage>
+}
