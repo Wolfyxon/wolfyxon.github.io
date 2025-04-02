5f51aa72fc91ff6c3ca575245f10ffd9c33c3e94
Wolfyxon <wolfyxon@gmail.com>
2025-04-02 16:58:42 +0200

    Added nintendo

---
+++ src/app/ose/nintendo/page.tsx
@@ -0,0 +1,28 @@
+import { Metadata } from "next";
+import OsePage from "../OsePage";
+import TextLink from "@/components/TextLink";
+
+export const metadata: Metadata = {
+    title: "Ogólnoświatowa Sieć Nintendo"
+}
+
+export default function PisOsePage() {
+    return <OsePage 
+            icon="/assets/media/img/ose/nintendo.png" 
+            title={metadata.title as string}
+            heading="Ta strona łamie prawa autorskie"
+            category="Copyright infringement"
+            dynamicCategory="PIRACY MUSIC FAN_GAMES">
+
+            <p>
+                Dostęp do tej strony został zablokowany ze względu na usługę Ochrona Gracza Nintendo, która jest włączona w Twojej konsoli.
+            </p>
+            <p>
+                Specjalny oddział Ninja został wysłany do twojej lokalizacji. <br />
+                Szacowany czas dotarcia: <strong>TODO</strong>
+            </p>
+
+            <p>Więcej o usłudze na <TextLink>https://www.nintendo.com/</TextLink>.</p>
+
+        </OsePage>
+}
