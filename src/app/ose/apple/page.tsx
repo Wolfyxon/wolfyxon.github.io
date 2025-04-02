b024382182e566975a22b63cb8f5c04ca8718b8c
Wolfyxon <wolfyxon@gmail.com>
2025-04-02 21:55:23 +0200

    Added apple

---
+++ src/app/ose/apple/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+import OsePage from "../OsePage";
+import TextLink from "@/components/TextLink";
+
+export const metadata: Metadata = {
+    title: "Ogólnourządzeniowa Sieć Apple"
+}
+
+export default function AppleOsePage() {
+    return <OsePage 
+            icon="/assets/media/img/ose/apple.png" 
+            title={metadata.title as string}
+            heading="Ta strona zawiera nieautoryzowane naprawy"
+            category="Unauthorized Repairs"
+            dynamicCategory="REPAIR TUTORIAL REPLACEMENT_PARTS">
+
+            <p>
+                Dostęp do tej strony został zablokowany ze względu na usługę Ochrona Urządzeń Apple, która jest włączona w naszych urządzeniach.
+            </p>
+
+            <p>Więcej o usłudze <TextLink>https://www.apple.com/</TextLink></p>
+
+        </OsePage>
+}
