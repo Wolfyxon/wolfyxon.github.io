7d7b60ded66c279083ad956c1a73167e30c53418
Wolfyxon <wolfyxon@gmail.com>
2025-04-02 20:41:02 +0200

    Added china

---
+++ src/app/ose/china/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+import OsePage from "../OsePage";
+import TextLink from "@/components/TextLink";
+
+export const metadata: Metadata = {
+    title: "中國安防網"
+}
+
+export default function EuOsePage() {
+    return <OsePage 
+            icon="/assets/media/img/ose/china.png"
+            title={metadata.title as string}
+            heading="本網站含有錯誤訊息"
+            category="Misinformation"
+            dynamicCategory="HONG_KONG DEMOCRACY FREEDOM UNITED_STATES">
+
+            <p>
+                由於您所在國家/地區啟用了中國公民保護服務，因此無法存取此頁面。
+            </p>
+
+            <p>有關該服務的更多信息，請訪問 <TextLink>https://english.www.gov.cn/</TextLink>.</p>
+
+        </OsePage>
+}
