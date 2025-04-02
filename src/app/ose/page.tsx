d5e40a94e557ce5e52fb2f0ee84bc1f9edbd5bbe
Wolfyxon <wolfyxon@gmail.com>
2025-04-02 18:37:07 +0200

    Added home page

---
+++ src/app/ose/page.tsx
@@ -0,0 +1,33 @@
+import * as fs from "fs";
+
+import "./page.css";
+
+const dir = "src/app/ose/";
+
+function getPages(): string[] {
+    const res: string[] = []
+
+    fs.readdirSync(dir).forEach((v) => {
+        if(fs.statSync(dir + v).isDirectory()) {
+            res.push(v);
+        }
+    });
+
+    return res;
+}
+
+export default function OseHomePage() {
+    return (
+        <>
+            <h1>Ta strona zawiera parodie OSE ☹</h1>
+            
+            <div id="links">
+                {getPages().map((v) => {
+                    return <a href={"./" + v} key={v}>{v}</a>
+                })}
+            </div>
+            
+            <a href="/">Strona główna</a>
+        </>
+    )
+}
