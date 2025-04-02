967395e231d3619624c8119063e13f6eb3d333f7
Wolfyxon <wolfyxon@gmail.com>
2025-04-02 18:16:35 +0200

    Added combine

---
+++ src/app/ose/combine/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+import OsePage from "../OsePage";
+import TextLink from "@/components/TextLink";
+
+export const metadata: Metadata = {
+    title: "Ogólnoziemska Sieć Kombinatu"
+}
+
+export default function ArstotzkaOsePage() {
+    return <OsePage 
+            icon="/assets/media/img/ose/combine.svg" 
+            title={metadata.title as string}
+            heading="Ta strona zawiera ruch oporu"
+            category="Resistance"
+            dynamicCategory="GORDON_FREEMAN ELI_VANCE">
+
+            <p>
+                Dostęp do tej strony został zablokowany ze względu na usługę Ochrona Obywatela Ziemi, która jest włączona na Twojej planecie.
+            </p>
+
+            <p>Więcej o usłudze na <TextLink>https://half-life.fandom.com/wiki/Combine</TextLink></p>
+
+        </OsePage>
+}
