a25b84e438a819ee30e2367d7851153071d7a518
Wolfyxon <wolfyxon@gmail.com>
2025-04-02 14:37:02 +0200

    Added rust

---
+++ src/app/ose/rust/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+import OsePage from "../OsePage";
+import TextLink from "@/components/TextLink";
+
+export const metadata: Metadata = {
+    title: "Ogólnojęzykowa Sieć Pamięci"
+}
+
+export default function PisOsePage() {
+    return <OsePage 
+            icon="/assets/media/img/icons/software/lang/rust.png" 
+            title={metadata.title as string}
+            heading="Ta strona zawiera niebezpieczną pamięć"
+            category="Memory Unsafe"
+            dynamicCategory="USE_AFTER_FREE NULL C C++">
+
+            <p>
+                Dostęp do tej strony został zablokowany ze względu na usługę Ochrona Pamięci Programu, która jest włączona w Twoim języku.
+            </p>
+
+            <p>Więcej o usłudze na <TextLink>https://www.rust-lang.org/</TextLink>.</p>
+
+        </OsePage>
+}
