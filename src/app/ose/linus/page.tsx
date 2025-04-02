7648a7b085ee03fd94ab58519ffdb51729eb61cf
Wolfyxon <wolfyxon@gmail.com>
2025-04-02 14:40:35 +0200

    Added linus

---
+++ src/app/ose/linus/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+import OsePage from "../OsePage";
+import TextLink from "@/components/TextLink";
+
+export const metadata: Metadata = {
+    title: "Ogólnokernelowa Sieć AntyNVIDiowa"
+}
+
+export default function LinusOsePage() {
+    return <OsePage 
+            icon="/assets/media/img/ose/linus.webp" 
+            title={metadata.title as string}
+            heading="Ta strona zawiera NVIDIe"
+            category="NVIDIA"
+            dynamicCategory="NVIDIA">
+
+            <p>
+                Dostęp do tej strony został zablokowany ze względu na usługę Ochrona Linux, która jest włączona w twoim kernelu.
+            </p>
+
+            <p>Więcej o usłudze na <TextLink>https://kernel.org</TextLink>.</p>
+
+        </OsePage>
+}
