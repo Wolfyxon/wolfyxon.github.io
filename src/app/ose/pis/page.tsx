8c03a3bb1e40bc5b8032e2e0b9a47b536fe4811e
Wolfyxon <wolfyxon@gmail.com>
2025-04-02 12:56:12 +0200

    Locally storing PIS logo

--- src/app/ose/pis/page.tsx
+++ src/app/ose/pis/page.tsx
@@ -2,7 +2,7 @@ import OsePage from "../OsePage";
 
 export default function PisOsePage() {
     return <OsePage 
-            icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUn8e5hpB_Kmvd_af_5XBR-3hT5DLF4qp6qw&ss" 
+            icon="/assets/media/img/ose/pis.png" 
             title="Ogólnopolska Sieć Antylewacyjna"
             heading="Ta strona zawiera lewactwo"
             category="Lewactwo"
