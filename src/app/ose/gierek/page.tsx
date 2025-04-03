5dfe3f9b034afed88cf5d1175faeb451512a4dc0
Wolfyxon <wolfyxon@gmail.com>
2025-04-03 22:57:40 +0200

    Added gierek

---
+++ src/app/ose/gierek/page.tsx
@@ -0,0 +1,26 @@
+import { Metadata } from "next";
+import OsePage from "../OsePage";
+import TextLink from "@/components/TextLink";
+
+export const metadata: Metadata = {
+    title: "Ogólnogierkowa Sieć Robotnicza"
+}
+
+export default function GierekOsePage() {
+    return <OsePage 
+            icon="/assets/media/img/ose/gierek.jpg" 
+            title={metadata.title as string}
+            heading="Ta strona zawiera gierki"
+            category="Games"
+            dynamicCategory="GAMES GAMING VIDEO_GAMES">
+
+            <p>
+                Ładnie to tak w gierki grać na lekcji kolego?
+            </p>
+
+            <p>
+                Do roboty a nie.
+            </p>
+
+        </OsePage>
+}
