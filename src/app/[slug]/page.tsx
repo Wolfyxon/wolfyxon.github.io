3174c0639a5f46777e79960b343b6e00c1aaaa0f
Wolfyxon <wolfyxon@gmail.com>
2025-04-28 17:18:12 +0200

    Better redirect system

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,19 +1,57 @@
 import Redirect from "@/components/Redirect";
 
+type RedirectEntry = {
+    aliases: string[],
+    url: string
+}
+
+/*
 const redirects: {[slug: string]: string} = {
     "centerme": "/CenterMe",
     "discord": "https://discord.com/invite/RztUGCK",
     "Discord": "https://discord.com/invite/RztUGCK",
     "github": "https://github.com/wolfyxon",
     "GitHub": "https://github.com/wolfyxon"
+}*/
+
+const redirects: RedirectEntry[] = [
+    {
+        aliases: ["centerme", "Centerme", "CenterME", "centreme", "CentreMe"],
+        url: "https://wolfyxon.github.io/CenterMe",
+    },
+    {
+        aliases: ["discord", "Discord"],
+        url: "https://discord.com/invite/RztUGCK"
+    },
+    {
+        aliases: ["github", "Github", "GitHub"],
+        url: "https://github.com/wolfyxon"
+    }
+]
+
+function getRedirect(slug: string): RedirectEntry | null {
+    for(const r of redirects) {
+        if(r.aliases.includes(slug)) {
+            return r;
+        }
+    }
+    
+    return null;
 }
 
 export async function generateStaticParams() {
-    return Object.keys(redirects).map((key) => { return { slug: key }});
+    const res: {slug: string}[] = [];
+
+    redirects.forEach((v) => {
+        v.aliases.forEach((v) => res.push({slug: v}));
+    });
+    
+    return res;
 }
 
 export default async function SlugRedirect(data: {params: any}) {
     const slug = (await data.params).slug as string;
+    const redirect = getRedirect(slug);
 
-    return <Redirect url={redirects[slug]} />;
+    return <Redirect url={redirect!.url} />;
 }
