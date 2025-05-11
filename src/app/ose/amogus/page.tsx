be2d6a1987a72f2f8ea75a294e06afa54a29093a
Wolfyxon <wolfyxon@gmail.com>
2025-05-11 22:01:19 +0200

    Added amogus page

---
+++ src/app/ose/amogus/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+import OsePage from "../OsePage";
+import TextLink from "@/components/TextLink";
+
+export const metadata: Metadata = {
+    title: "Ogólnogalaktyczna Sieć Załogowa"
+}
+
+export default function AmogusOsePage() {
+    return <OsePage 
+            icon="/assets/media/img/ose/amogus.webp" 
+            title={metadata.title as string}
+            heading="Ta strona jest sus"
+            category="Impostor"
+            dynamicCategory="SUS SUSSY IMPOSTOR">
+
+            <p>
+                Dostęp do tej strony został zablokowany ze względu na usługę Ochrona Członka Załogi, która jest włączona w Twoim statku.
+            </p>
+
+            <p>Więcej o usłudze na <TextLink>https://www.innersloth.com/games/among-us/</TextLink></p>
+
+        </OsePage>
+}
