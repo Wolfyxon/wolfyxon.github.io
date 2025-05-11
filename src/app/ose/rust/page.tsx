9a93e190d6ae0721e7ed4da6e714aeb7b63895f3
Wolfyxon <wolfyxon@gmail.com>
2025-05-11 22:27:06 +0200

    Better heading

--- src/app/ose/rust/page.tsx
+++ src/app/ose/rust/page.tsx
@@ -10,7 +10,7 @@ export default function RustOsePage() {
     return <OsePage 
             icon="/assets/media/img/icons/software/lang/rust.png" 
             title={metadata.title as string}
-            heading="Ta strona zawiera niebezpieczną pamięć"
+            heading="Ta strona zawiera niewłaściwą pamięć"
             category="Memory Unsafe"
             dynamicCategory="USE_AFTER_FREE NULL BUFFER_OVERFLOW MEMORY_CORRUPTION C C++">
 
