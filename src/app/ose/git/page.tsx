dd0ed9b32108f52ad4cd200bdc9242d9b3f5fa6e
Wolfyxon <wolfyxon@gmail.com>
2025-05-11 22:20:12 +0200

    Added git page

---
+++ src/app/ose/git/page.tsx
@@ -0,0 +1,31 @@
+import { Metadata } from "next";
+import OsePage from "../OsePage";
+import TextLink from "@/components/TextLink";
+
+export const metadata: Metadata = {
+    title: "Ogólnokodowa Sieć Wersji"
+}
+
+const lts = "<<<<<<<";
+const gts = ">>>>>>>";
+
+export default function GitOsePage() {
+    return <OsePage 
+            icon="/assets/media/img/icons/software/tools/git.svg" 
+            title={metadata.title as string}
+            heading="Ta strona zawiera merge conflict"
+            category="Merge Conflict"
+            dynamicCategory="MERGE_CONFLICT GIT_PULL GIT_MERGE">
+
+            <p>
+                {lts} HEAD <br />
+                Dostęp do tej strony został zablokowany ze względu na usługę Ochrona Kontroli Wersji, która jest włączona w Twoim kodzie. <br />
+                ======= <br />
+                Dostęp do tej strony został zablokowany ze względu na usługę Ochrona Kontroli Kodu, która jest włączona w twoim narzędziu Git. <br />
+                {gts} main
+            </p>
+
+            <p>Więcej o usłudze na <TextLink>https://git-scm.com/</TextLink></p>
+
+        </OsePage>
+}
