0697b4f29feff9dd1dccabc8beb10d2b9bcd75c8
Wolfyxon <wolfyxon@gmail.com>
2025-05-11 22:30:23 +0200

    Added python page

---
+++ src/app/ose/python/page.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from "next";
+import OsePage from "../OsePage";
+import TextLink from "@/components/TextLink";
+
+export const metadata: Metadata = {
+    title: "Ogólnojęzykowa Sieć Pytonów"
+}
+
+export default function PythonaOsePage() {
+    return <OsePage 
+            icon="/assets/media/img/icons/software/lang/python.svg" 
+            title={metadata.title as string}
+            heading="Ta strona zawiera za dużo spacji"
+            category="Invalid indentation"
+            dynamicCategory="INVALID_INDENTATION INVALID_TABS INVALID_SPACE">
+
+            <p>
+                Dostęp do tej strony został zablokowany ze względu na usługę Ochrona Języka Python, która jest włączona w Twoim języku.
+            </p>
+
+            <p>Więcej o usłudze na <TextLink>https://www.python.org/</TextLink></p>
+
+        </OsePage>
+}
