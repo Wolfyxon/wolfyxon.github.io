e25db7bd710a09f575c5b750b43c7f6c620cbff2
Wolfyxon <wolfyxon@gmail.com>
2025-05-18 22:59:09 +0200

    Added name tooltips

--- src/app/lighter/result/LighterResult.tsx
+++ src/app/lighter/result/LighterResult.tsx
@@ -7,6 +7,7 @@ export default function LighterResult(props: {candidate: Candidate, percent: num
         <div className="candidate-result">
             <div className="candidate-icon-container">
                 <img src={props.candidate.icon} className="candidate-icon" />
+                <div className="candidate-name">{props.candidate.name}</div>
             </div>
             <div className="candidate-bar">
                 <div className="candidate-bar-inner" style={{width: `${props.percent}%`}}></div>
