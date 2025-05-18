ee14c6611d13bd197abda948bada12ec9eba5ac5
Wolfyxon <wolfyxon@gmail.com>
2025-05-18 18:58:10 +0200

    Styled the results

--- src/app/lighter/test/LighterResult.tsx
+++ src/app/lighter/test/LighterResult.tsx
@@ -1,5 +1,7 @@
 import { Candidate } from "../Lighter";
 
+import "./style.css";
+
 export default function LighterResult(props: {candidate: Candidate, percent: number}) {
     return (
         <div className="candidate-result">
@@ -7,7 +9,7 @@ export default function LighterResult(props: {candidate: Candidate, percent: num
                 <img src={props.candidate.icon} className="candidate-icon" />
             </div>
             <div className="candidate-bar">
-                <div className="candidate-bar-inner"></div>
+                <div className="candidate-bar-inner" style={{width: `${props.percent}%`}}></div>
             </div>
             <div className="candidate-percent">
                 {Math.round(props.percent)}%
