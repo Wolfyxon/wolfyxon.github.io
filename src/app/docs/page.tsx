285423a6a57477ab480a157b545803b771aa74dd
Wolfyxon <wolfyxon@gmail.com>
2025-07-02 17:43:20 +0200

    Docs home page

---
+++ src/app/docs/page.tsx
@@ -0,0 +1,29 @@
+import Widget from "@/components/Widget/Widget";
+import ArticlePage from "@/layouts/ArticlePage/ArticlePage";
+import Page from "@/layouts/Page";
+import { getDocs } from "./docs";
+
+export default async function DocsHomePage() {
+    return (
+        <Page homeUrl="" category="Docs">
+            <ArticlePage title="Documentation">
+                <h1>Welcome to my documentation</h1>
+                <p>
+                    Here you can find some important information about my projects
+                    and me.
+                </p>
+
+                <div id="doc-list">
+                    {
+                        (await getDocs()).map((doc) => {
+                            return <Widget title={doc.title} url={doc.slug} key={doc.title}>
+                                {doc.description || "No Description"}
+                            </Widget>
+                        })
+                    }
+                </div>
+
+            </ArticlePage>
+        </Page>
+    )
+}
