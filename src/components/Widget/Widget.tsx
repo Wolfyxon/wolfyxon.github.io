603f13749af59bf1658a0a99ef6ecdd7694d22ae
Wolfyxon <wolfyxon@gmail.com>
2025-07-02 17:34:27 +0200

    Added icon

--- src/components/Widget/Widget.tsx
+++ src/components/Widget/Widget.tsx
@@ -5,8 +5,13 @@ import "./Widget.css";
 export default function Widget(props: {title: string, url?: string, children: ReactNode}) {
     const body = (
     <>
-        <div className="widget-title">
-            {props.title}
+        <div className="widget-title-container">
+            <div className="widget-title">{props.title}</div>
+            
+            {
+                props.url ? <img className="widget-title-icon" src="/assets/media/img/icons/google/open_in_new.svg" />
+                : null
+            }
         </div>
         
         <div className="widget-content">
