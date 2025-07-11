537458507142ee60df57c438837368cadfe0c827
Wolfyxon <wolfyxon@gmail.com>
2025-07-11 21:55:35 +0200

    Ability to center the fancy heading

--- src/components/FancyHeading/FancyHeading.tsx
+++ src/components/FancyHeading/FancyHeading.tsx
@@ -1,8 +1,14 @@
 import "./FancyHeading.css";
 
-export default function FancyHeading(data: {children: any}) {
+export default function FancyHeading(data: {children: any, centered?: boolean}) {
+    let className = "fancy-h";
+
+    if(data.centered) {
+        className += " fancy-h-center"
+    }
+    
     return (
-        <h1 className="fancy-h">
+        <h1 className={className}>
             <div>
                 {data.children}
             </div>
