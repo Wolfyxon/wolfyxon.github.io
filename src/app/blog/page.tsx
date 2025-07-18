df53348e597b5f10ec6b93c9135e9c71ffa302f3
Wolfyxon <wolfyxon@gmail.com>
2025-07-18 21:11:33 +0200

    Added archive warning

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,6 +5,8 @@ import PostCard from "./PostCard/PostCard";
 import "../css/animations.css";
 import "./blog.css";
 import Page from "@/layouts/Page";
+import Warning from "@/components/Warning/Warning";
+import { DISCORD_INVITE } from "@/globalData";
 
 export const metadata: Metadata = {
     title: "My blog",
@@ -14,6 +16,16 @@ export const metadata: Metadata = {
 export default async function Blog() {
     return (
         <Page category="Blog">
+            <Warning>
+                <h1>Archived</h1>
+                <p>
+                    The blog has been archived and will no longer be updated.
+                </p>
+                <p>
+                    For similar articles, see <a href="/docs">the documentation</a> <br/>
+                    For announcements, my <a href={DISCORD_INVITE}>Discord server</a>.
+                </p>
+            </Warning>
             <h1>Posts</h1>
 
             <div id="posts">
