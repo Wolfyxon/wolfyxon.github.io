b797b80d426dbfcf9ec6c900969d9f40918544a1
Wolfyxon <wolfyxon@gmail.com>
2025-09-10 18:12:27 +0200

    Better footer links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,40 @@
+import { ReactNode } from "react";
 import "./Footer.css"
+import { DISCORD_INVITE } from "@/globalData";
 
 export default function Footer() {
     return (    
         <footer>
-            <p>
-                <a href="https://github.com/Wolfyxon/wolfyxon.github.io" target="_blank">Source code</a>
-                <a href="https://github.com/Wolfyxon/wolfyxon.github.io/issues/new" target="_blank">Report a bug or propose a change</a>
-                <a href="https://github.com/Wolfyxon/wolfyxon.github.io/issues">Issues</a>
-            </p>
-            <p>
-                <a href="mailto:wolfyxon@gmail.com" translate="no">wolfyxon@gmail.com</a>
-            </p>
+            <div id="footer-columns">
+                <Column title="Repository">
+                    <a href="https://github.com/Wolfyxon/wolfyxon.github.io">Source code</a>
+                    <a href="https://github.com/Wolfyxon/wolfyxon.github.io/issues">Bugs & suggestions</a>
+                </Column>
 
+                <Column title="Super secret">
+                    <a href="/ose">OSE</a>
+                    <a href="/archive/lighter/tgdod">Lighter</a>
+                    <a href="/stuff/ttt">Tic Tac Toe</a>
+                    <a href="/3ds-web-stuff">3DS Web Stuff</a>
+                </Column>
+
+                <Column title="Contact">
+                    <a href="mailto:wolfyxon@gmail.com" translate="no">wolfyxon@gmail.com</a>
+                    <a href={DISCORD_INVITE}>Discord</a>
+                </Column>
+            </div>
+            
             <p>This website is licensed under <a href="https://github.com/Wolfyxon/wolfyxon.github.io/blob/main/LICENSE">GPL-3.0</a>.</p>
             <p>Made by Wolfyxon.</p>
         </footer>
     );
+}
+
+function Column(props: {title: string, children: ReactNode}) {
+    return (
+        <div className="footer-column">
+            <div className="footer-column-title">{props.title}</div>
+            {props.children}
+        </div>
+    )
 }
