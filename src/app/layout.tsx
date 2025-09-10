ef30db2376ddb9e9b0eb660c4673e76f26afe72d
Wolfyxon <wolfyxon@gmail.com>
2025-09-10 13:20:38 +0200

    Better preconnect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
     <html lang="en">
       <head>
         <meta name="google-site-verification" content="CEp8re5xPxShHGl3Z04NXVKOKE-OgXujmu6jYVBIXqE" />
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"></link>
 
         {/* css @import() broke lol */}
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"></link>
