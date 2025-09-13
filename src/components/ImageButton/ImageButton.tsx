be5b87f6b8a6d5a3a77bd9e43410c4c5a12c37c6
Wolfyxon <wolfyxon@gmail.com>
2025-09-13 11:50:11 +0200

    Using them for ImageButton

--- src/components/ImageButton/ImageButton.tsx
+++ src/components/ImageButton/ImageButton.tsx
@@ -1,22 +1,15 @@
+import { ElmBase, classJoin } from "@/utils";
 import "./style.css";
 
 export default function ImageButton(props: {
     img: string, 
-    className?: string, 
-    id?: string, 
     label?: string, 
     ariaLabel?: string, 
     onClick?: () => any,
     disabled?: boolean
-}) {
-    let className = "img-button";
-    
-    if(props.className) {
-        className += " " + props.className;
-    }
-
+} & ElmBase) {
     return (
-        <button onClick={props.onClick} id={props.id} className={className} aria-label={props.ariaLabel ?? props.label} disabled={props.disabled}>
+        <button onClick={props.onClick} id={props.id} className={classJoin("img-button", props.className)} aria-label={props.ariaLabel ?? props.label} disabled={props.disabled}>
             <div className="img-button-img-container">
                 <img src={props.img} className="img-button-img" alt="icon" />
                 <span className="img-button-label">{props.label}</span>
