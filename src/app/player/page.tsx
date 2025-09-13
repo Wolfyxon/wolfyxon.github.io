8720e7a1dfa136b33897e86e333f52b817a0c564
Wolfyxon <wolfyxon@gmail.com>
2025-09-13 12:27:01 +0200

    Better description

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -13,7 +13,7 @@ export default function PlayerPage() {
     return (
         <Page category="Player">
             <h1>Player</h1>
-            <p>A fading audio player I made to better handle music at school ceremonies.</p>
+            <p>A fading audio player I made to better handle music at school events so the music isn't just unprofessionally interrupted.</p>
 
             <PlayerPageClient />
         </Page>
