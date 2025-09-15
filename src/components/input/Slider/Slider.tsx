06239738bb61d204471719b808881d5c1296f0a0
Wolfyxon <wolfyxon@gmail.com>
2025-09-15 13:46:16 +0200

    Init <Slider>

---
+++ src/components/input/Slider/Slider.tsx
@@ -0,0 +1,55 @@
+import { ChangeEvent, useState } from "react"
+
+import "./style.css";
+
+export default function Slider(props: {
+    label?: string,
+    ariaLabel?: string, 
+    min?: number,
+    max?: number,
+    onChange?: ((val: number) => void)
+    value?: number
+}) {
+    const [value, setValue] = useState(props.value ?? (props.min ?? 0));
+
+    function changed(e: ChangeEvent) {
+        const inp = e.target! as HTMLInputElement;
+        
+        const num = parseFloat(inp.value);
+
+        if(isNaN(num)) {
+            setValue(value);
+            return;
+        }
+
+        setValue(num);
+
+        if(props.onChange) {
+            props.onChange(num);
+        }
+    }
+
+    return (
+        <div>
+            <label className="slider-inner">
+                {props.label}
+
+                <input type="range" 
+                    aria-label={props.ariaLabel}
+                    value={value}
+                    onChange={changed}
+                    min={props.min}
+                    max={props.max}
+                />
+
+                <input type="number" 
+                    aria-label={props.ariaLabel}
+                    value={value}
+                    onChange={changed}
+                    min={props.min}
+                    max={props.max}
+                />
+            </label>
+        </div>
+    )
+}
