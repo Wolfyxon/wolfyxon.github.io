88851fe88341607b271143ad93c9003be3c1a4c8
Wolfyxon <wolfyxon@gmail.com>
2025-09-16 15:47:56 +0200

    Flat checkbox style

--- src/components/input/Checkbox/Checkbox.tsx
+++ src/components/input/Checkbox/Checkbox.tsx
@@ -1,7 +1,7 @@
 import { ChangeEvent, useState } from "react";
 import "./style.css";
 
-export default function Checkbox(props: {label: string, checked?: boolean, onChange?: (value: boolean) => any}) {
+export default function Checkbox(props: {label: string, checked?: boolean, flat?: boolean, onChange?: (value: boolean) => any}) {
     const [check, setCheck] = useState(props.checked ?? false);
 
     function change(e: ChangeEvent) {
@@ -14,8 +14,10 @@ export default function Checkbox(props: {label: string, checked?: boolean, onCha
         }
     }
 
+    const flat = props.flat ? "flat" : "";
+
     return (
-        <label className="checkbox-container">
+        <label className={`checkbox-container ${flat}`}>
             <div className="checkbox-container-inner">
                 <input type="checkbox" aria-label={props.label} defaultChecked={check} onChange={change} />
                 <div>{props.label}</div>
