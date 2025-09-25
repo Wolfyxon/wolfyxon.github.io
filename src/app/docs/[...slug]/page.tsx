b9ea1e5947a5d504ffa1d6ea5f6081fa52bca4b3
Wolfyxon <wolfyxon@gmail.com>
2025-09-25 11:58:46 +0200

    Link to go back to the home doc page

--- src/app/docs/[...slug]/page.tsx
+++ src/app/docs/[...slug]/page.tsx
@@ -2,6 +2,7 @@ import { getDocBySlug, getDocs } from "../docs";
 
 import Page from "@/layouts/Page";
 import ArticlePage from "@/layouts/ArticlePage/ArticlePage";
+import TextIcon from "@/components/TextIcon/TextIcon";
 
 const REPO_URL = "https://github.com/Wolfyxon/wolfyxon.github.io/tree/main/public/docs";
 
@@ -27,6 +28,13 @@ export default async function DocPage(props: {params: any}) {
 
     const mdFile = slug.join("/") + ".md";
 
+    const header = (
+        <a href="/blog">
+            <TextIcon src="/assets/media/img/icons/google/arrow-back.svg" alt="Back arrow" />
+            Back to all docs
+        </a>
+    );
+
     const footer = (
         <div>
             <a href={`${REPO_URL}/${mdFile}`}>See on GitHub</a> | <a href={`/docs/${mdFile}`}>Plain text</a>
@@ -35,7 +43,7 @@ export default async function DocPage(props: {params: any}) {
 
     return (
         <Page category="Docs" homeUrl="/docs">
-            <ArticlePage title={doc.title} footerElement={footer}>
+            <ArticlePage title={doc.title} headerElement={header} footerElement={footer}>
                 {doc.markdown}
             </ArticlePage>
         </Page>
