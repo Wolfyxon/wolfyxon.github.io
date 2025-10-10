00a6f9491c76f73dbac111ed9d1f268e6fa07910
Wolfyxon <wolfyxon@gmail.com>
2025-10-10 12:23:05 +0200

    Async support

--- src/components/FileUpload/FileUpload.tsx
+++ src/components/FileUpload/FileUpload.tsx
@@ -7,12 +7,14 @@ import "./style.css";
 
 export const UPLOAD_NOTE_OFFLINE = "No stable internet required. Everything is handled locally in your browser";
 
+export type FileUploadError = boolean | string | string[] | undefined | null | void;
+
 export default function FileUpload(props: {
     accept: string,
     prefix?: string,
     note?: string,
     global?: boolean,
-    callback: (files: FileList) => boolean | string | string[] | undefined | null | void
+    callback: ((files: FileList) => FileUploadError | Promise<FileUploadError>)
 } & ElmBase) {
     const [error, setError] = useState("");
     const ref = useRef<HTMLDivElement>(null);
@@ -29,8 +31,12 @@ export default function FileUpload(props: {
         inp.files = null;
     }
 
-    function filesDropped(files: FileList) {
-        const result = props.callback(files);
+    async function filesDropped(files: FileList) {
+        let result = props.callback(files);
+
+        if(result instanceof Promise) {
+            result = await result;
+        }
 
         if(result != true && result !== null && result !== undefined) {       
             const resType = typeof(result);
