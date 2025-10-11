3424da6b70c23d5940d3663f1f397b2a889d3b0a
Wolfyxon <wolfyxon@gmail.com>
2025-10-11 21:28:45 +0200

    Better description

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -7,14 +7,14 @@ import "@/css/pageInset.css";
 
 export const metadata: Metadata = {
     title: "Player",
-    description: "Smooth audio player thing"
+    description: "An event handling tool with slideshow functionality and better audio handling."
 }
 
 export default function PlayerPage() {
     return (
         <Page category="Player">
             <h1>Player</h1>
-            <p>A fading audio player I made to better handle music at school events so the music isn't just unprofessionally interrupted.</p>
+            <p>{metadata.description} Made for my school.</p>
 
             <PlayerPageClient />
         </Page>
