901df4b7c359819e38431d5e3cbfe539cd92055f
Wolfyxon <wolfyxon@gmail.com>
2025-10-12 11:50:18 +0200

    Video preview

--- src/app/slideshow/client.tsx
+++ src/app/slideshow/client.tsx
@@ -6,7 +6,10 @@ import SlideShowControls, { SlideData } from "./controls/SlideShowControls";
 export default function SlideShowPageClient() {
     
     const [currentlide, setCurrentSlide] = useState<SlideData | null>(null);
-    const previewRef = useRef<HTMLImageElement>(null);
+
+    const previewRef = useRef<HTMLDivElement>(null);
+    const imgRef = useRef<HTMLImageElement>(null);
+    const videoRef = useRef<HTMLVideoElement>(null);
 
     function fullscreen() {
         previewRef.current?.requestFullscreen();
@@ -14,15 +17,37 @@ export default function SlideShowPageClient() {
     
     return (
         <div className="slideshow-page-container">
-            <div className="slideshow-preview">
-                <img 
-                    alt="Slide" 
-                    className="slideshow-preview-img"
-                    width={400}
-                    height={400}
-                    src={currentlide?.src ?? "/assets/media/img/maxwell.jpg"}
-                    ref={previewRef}
-                />
+            <div className="slideshow-preview" ref={previewRef}>
+                {
+                    currentlide && currentlide.blob.type.startsWith("video/") ?
+                    (
+                        <video
+                            style={currentlide && currentlide.blob.type.startsWith("video/") ? undefined : {
+                                display: "none"
+                            }}
+                            width={400}
+                            height={400}
+                            ref={videoRef}
+                        >
+                            <source type={currentlide?.blob.type} src={currentlide?.src} />        
+                            Video not supported
+                        </video>
+                    ) :
+                    (
+                        <img
+                            style={currentlide && currentlide.blob.type.startsWith("video/") ? {
+                                display: "none"
+                            } : undefined}
+                            alt="Slide" 
+                            className="slideshow-preview-img"
+                            width={400}
+                            height={400}
+                            src={currentlide?.src ?? "/assets/media/img/maxwell.jpg"}
+                            ref={imgRef}
+                        />
+                    )
+                }
+
             </div>
             
             <SlideShowControls
