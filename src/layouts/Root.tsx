ebe2e85f2d74d94640844e88c5ffc062f9a37775
Wolfyxon <wolfyxon@gmail.com>
2025-11-24 14:54:47 +0100

    Added some meta tags to prevent AI training

--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -11,6 +11,9 @@ export default function Root(props: {children: [HeadComponent, BodyComponent]}) 
         <html lang="en">
             <head>
                 <meta name="google-site-verification" content="CEp8re5xPxShHGl3Z04NXVKOKE-OgXujmu6jYVBIXqE" />
+                <meta name="CCBot" content="nofollow" />
+                <meta name="robots" content="noai, noimageai" />
+                <meta name="tdm-reservation" content="1" />
 
                 <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous"></link>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"></link>
