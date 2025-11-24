d0aad485408cae86ef82472148a313c77dad7a97
Wolfyxon <wolfyxon@gmail.com>
2025-11-24 14:57:44 +0100

    Head tags cleanup

--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -2,6 +2,22 @@
 export type HeadComponent = React.ReactElement<React.HTMLAttributes<HTMLHeadElement>, 'head'>
 export type BodyComponent = React.ReactElement<React.HTMLAttributes<HTMLBodyElement>, 'body'>
 
+const GLOBAL_HEAD = (<>
+    <meta name="google-site-verification" content="CEp8re5xPxShHGl3Z04NXVKOKE-OgXujmu6jYVBIXqE" />
+    
+    <meta name="CCBot" content="nofollow" />
+    <meta name="robots" content="noai, noimageai" />
+    <meta name="tdm-reservation" content="1" />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+
+    {/* css @import() broke lol */}
+    <link 
+        rel="stylesheet" 
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+    />
+</>);
 
 export default function Root(props: {children: [HeadComponent, BodyComponent]}) {
     const head = props.children[0];
@@ -10,17 +26,7 @@ export default function Root(props: {children: [HeadComponent, BodyComponent]}) 
     return (
         <html lang="en">
             <head>
-                <meta name="google-site-verification" content="CEp8re5xPxShHGl3Z04NXVKOKE-OgXujmu6jYVBIXqE" />
-                <meta name="CCBot" content="nofollow" />
-                <meta name="robots" content="noai, noimageai" />
-                <meta name="tdm-reservation" content="1" />
-
-                <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous"></link>
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"></link>
-
-                {/* css @import() broke lol */}
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"></link>
-
+                {GLOBAL_HEAD}
                 {head.props.children}
             </head>
             <body>
