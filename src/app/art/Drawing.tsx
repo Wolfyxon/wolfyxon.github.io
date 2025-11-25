dbf30484fd47f317ad891f757141e8fc84780d6f
Wolfyxon <wolfyxon@gmail.com>
2025-11-25 22:08:34 +0100

    Click oob to close

--- src/app/art/Drawing.tsx
+++ src/app/art/Drawing.tsx
@@ -3,7 +3,7 @@
 import ImageButton from "@/components/input/ImageButton/ImageButton";
 import { capitalize } from "@/util/string";
 import { MONTHS } from "@/util/time";
-import { useEffect, useState } from "react";
+import { MouseEvent, useEffect, useRef, useState } from "react";
 
 export type DrawingData = {
     title: string,
@@ -16,6 +16,7 @@ export default function Drawing(props: {data: DrawingData}) {
     const date = new Date(data.date);
 
     const [isOpenFullscreen, setOpenFullscreen] = useState(false);
+    const fullscreenRef = useRef<HTMLDivElement>(null);
 
     function open() {
         if(isOpenFullscreen) return;
@@ -25,6 +26,13 @@ export default function Drawing(props: {data: DrawingData}) {
 
     function close() {
         setOpenFullscreen(false);
+    }
+
+    function fullscreenClicked(e: MouseEvent<HTMLDivElement>) {
+        console.log(e.target);
+        if(e.target == fullscreenRef.current) {
+            close();
+        }
     }
 
     useEffect(() => {
@@ -37,7 +45,12 @@ export default function Drawing(props: {data: DrawingData}) {
 
     return (
         <div className="drawing" onClick={open}>
-            <div className="drawing-fullscreen" style={isOpenFullscreen ? {visibility: "visible", opacity: "1"} : undefined}>
+            <div
+                className="drawing-fullscreen" 
+                style={isOpenFullscreen ? {visibility: "visible", opacity: "1"} : undefined}
+                onClick={fullscreenClicked}
+                ref={fullscreenRef}
+            >
                 <img
                     src={data.src}
                     alt={data.title} 
