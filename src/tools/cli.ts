41af8f994cb2f57d7efc6e05d28b365da73fc500
Wolfyxon <wolfyxon@gmail.com>
2025-12-02 22:42:57 +0100

    Performing update before build

--- src/tools/cli.ts
+++ src/tools/cli.ts
@@ -1,10 +1,12 @@
-import { updateAiUserAgents } from "./robotsTool";
+import { checkedUpdateAiUserAgents, updateAiUserAgents } from "./robotsTool";
 
 async function main() {
     console.log("CLI running");
     
     if(process.argv.includes("--update-robots")) {
         updateAiUserAgents();
+    } else if(process.argv.includes("--check-update-robots")) {
+        checkedUpdateAiUserAgents();
     }
 }
 
