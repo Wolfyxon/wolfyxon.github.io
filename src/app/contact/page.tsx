5f57b79a47e3fee5a92abf207f409ce16da93154
Wolfyxon <wolfyxon@gmail.com>
2026-02-13 09:34:50 +0100

    Contact page update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,13 +9,22 @@ export default function ContactPage() {
     return (
         <Page category="contact">
             <ArticlePage title="Contact">
-                <h1>How to reach me</h1>
+                <h1>Ways to contact me</h1>
+                <ul>
+                    <li>Discord: <TextLink>{DISCORD_INVITE}</TextLink></li>
+                    <li>Email: <MailLink>wolfyxon@gmail.com</MailLink></li>
+                </ul>
+
+                <h1>Privacy & verification</h1>
                 <p>
-                    Email: <MailLink>wolfyxon@gmail.com</MailLink>
+                    If you'd like to encrypt your message, please consider encrypting it with
+                    my <a href="/resources/gpg/wolfyxon.txt">public key</a>. <br/>
+                    You may also use GPG/PGP to sign your message and prove your identity (if I have your public key already).
                 </p>
                 <p>
-                    Discord: <TextLink>{DISCORD_INVITE}</TextLink>
+                    All instructions can be found in the <a href="/docs/info/keys/">key guide</a>.
                 </p>
+
             </ArticlePage>
         </Page>
     )
