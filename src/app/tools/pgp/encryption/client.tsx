b537f1ab6af23a01d799fcc49f4ece3022f626da
Wolfyxon <wolfyxon@gmail.com>
2026-02-13 10:34:55 +0100

    Init encryption page

---
+++ src/app/tools/pgp/encryption/client.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import ImageButton from "@/components/input/ImageButton/ImageButton";
+
+export default function EncryptionPageClient(props: {myKey: string}) {
+    return (<>
+        <h1>Encrypt a message using OpenPGP</h1>
+        <div id="sides">
+            <div id="side-message">
+                <h2>Encryption</h2>
+                
+                <label htmlFor="area-text">Your message</label>
+                <textarea id="area-message"></textarea>
+
+                <ImageButton img="/assets/img/icons/google/check.svg" label="Encrypt" />
+            </div>
+            <div id="side-receipents">
+                <h2>Receipents</h2>
+            </div>
+        </div>
+    </>);
+}
---
+++ src/app/tools/pgp/encryption/page.tsx
@@ -0,0 +1,15 @@
+import Page from "@/layouts/Page";
+import * as fs from "node:fs";
+import EncryptionPageClient from "./client";
+
+import "./style.css";
+
+const MY_KEY = fs.readFileSync("public/resources/publicKeys/wolfyxon.txt").toString("utf-8");
+
+export default function EncryptionPage() {
+    return (
+        <Page category="Encryption">
+            <EncryptionPageClient myKey={MY_KEY} />
+        </Page>
+    );
+}
