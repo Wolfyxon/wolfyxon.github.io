69be5433d3562aafcfa0486adf43b380a86930d4
Wolfyxon <wolfyxon@gmail.com>
2026-02-13 13:01:54 +0100

    Init accordion buttons

--- src/components/input/Accordion/Accordion.tsx
+++ src/components/input/Accordion/Accordion.tsx
@@ -5,9 +5,16 @@ import { ReactNode, useState } from "react";
 import "./style.css";
 import { ElmBase, classJoin } from "@/util/dom";
 
+export type AccordionButton = {
+    icon: string,
+    name: string,
+    onClick: () => any
+}
+
 export default function Accordion(props: {
     title: string, 
     open?: boolean, 
+    buttons?: AccordionButton[],
     children: ReactNode
 } & ElmBase) {
     const [open, setOpen] = useState(props.open ?? false);
@@ -20,6 +27,22 @@ export default function Accordion(props: {
         <div className={classJoin(`accordion ${open ? "open" : ""}`, props.className)} id={props.id}>
             <button onClick={click} className="accordion-button">
                 <label>{props.title}</label>
+                <div className="accordion-buttons">
+                    {
+                        props.buttons ?
+                        <>
+                            <div></div>
+                            {
+                                props.buttons.map(btn =>
+                                    <button title={btn.name} onClick={btn.onClick}>
+                                        <img src={btn.icon}/>
+                                    </button>
+                                )
+                            }
+                        </>
+                        : null
+                    }
+                </div>
             </button>
             <div className="accordion-content">
                 {props.children}
