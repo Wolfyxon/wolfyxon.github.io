4f4fc034bf4d3e04755342a19cfcc0863b62fa04
Wolfyxon <wolfyxon@gmail.com>
2026-02-13 17:26:08 +0100

    Using page inset

--- src/app/tools/pgp/encryption/page.tsx
+++ src/app/tools/pgp/encryption/page.tsx
@@ -3,6 +3,7 @@ import * as fs from "node:fs";
 import EncryptionPageClient from "./client";
 
 import "./style.css";
+import "@/css/pageInset.css";
 
 const MY_KEY = fs.readFileSync("public/resources/publicKeys/wolfyxon.txt").toString("utf-8");
 
