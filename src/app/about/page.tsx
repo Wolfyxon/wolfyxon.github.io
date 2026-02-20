f39f72e4671c12faff331dfa0795e28e273dd370
Wolfyxon <wolfyxon@gmail.com>
2026-02-20 16:07:02 +0100

    About me update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,61 +19,36 @@ export default function About() {
                     </h2>
                     <p>
                         I'm a young programmer guy from Poland. <br />
-                        I've been coding since I was 9 years old and it truly is my passion. 
+                        I've been coding since I was 9 years old. 
                     </p>
                     <p>
                         The stuff I primarily make is tools, websites and games. 
-                        I code for fun and enjoy learning new technologies.
+                        I code for fun and enjoy learning new stuff.
                     </p>
                     <p>
-                        I'm also a beginner musician. 
-                        I compose digital music and sometimes play on a piano. 
-
-                        <noindex style={{color: "rgba(0, 0, 0, 0)"}}>
-                            I'm also autistic btw :3
-                        </noindex>
-                        
-                    </p>
-                </section>
-
-                <section>
-                    <h1 id="interests">Interests</h1>
-                    <p>
-                        Programming is my primary interest (a passion, actually) but I'm also into
-                        composing music, video and audio editing, science and psychology.
+                        I'm also into electronics. I like prototyping circuits and soldering. <br/>
+                        I write code for ESP32 and Arduino microcontrollers.
                     </p>
                     <p>
-                        I enjoy dealing with all kinds of software, especially if it provides a lot of control.
-                        That's why I use Linux and a lot of open source tools. 
-                    </p>
-                    <p>
-                        I like sandbox, strategy and shooter games.
-                    </p>
-                </section>
-
-                <section>
-                    <h1 id="how-i-code">How I code</h1>
-                    <p>
-                        I'm definitely on the craft-based side of programmers.
-                    </p>
-                    <p>
-                        I care a lot about code maintainability, structure, style, proper practices and performance.
-                        I would call myself a perfectionist in that <br/>
-                        (expect me to have a mental breakdown when looking at your code).
-                    </p>
-                    <p>
-                        Oh also don't look at my old projects please...
+                        From time to time I also compose music and try drawing.
+                        I'm quite good at editing videos too.                        
                     </p>
                 </section>
 
                 <section>
                     <h1 id="personality">Personality</h1>
                     <p>
-                        I'm a very introverted and a reserved person, but if you ask me the right questions expect me to
-                        suddenly talk a lot.
+                        I'm very introverted and don't talk much, but if you ask me the right questions expect
+                        everything to suddenly change.
                     </p>
+
                     <p>
-                        I care a lot about justice, respect and doing the right things.
+                        I value peace and silence and often like to disconnect from 
+                        the world and hyperfocus on something like coding.
+                        
+                        <noindex style={{color: "rgba(0, 0, 0, 0)"}}>
+                            I'm also autistic btw :3
+                        </noindex>
                     </p>
                 </section>
 
