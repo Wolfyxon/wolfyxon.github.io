68e4c5fdc713e1d74052f4ecb8f66500443de175
Wolfyxon <wolfyxon@gmail.com>
2026-07-31 12:07:38 +0200

    More vector functions

--- src/util/math.ts
+++ src/util/math.ts
@@ -36,3 +36,21 @@ export function angleTo(x1: number, y1: number, x2: number, y2: number) {
     
     return Math.atan2(dy, dx);
 }
+
+export function isColliding(x1: number, y1: number, w1: number, h1: number, x2: number, y2: number, w2: number, h2: number) {
+    const left1 = x1;
+    const right1 = x1 + w1;
+    const top1 = y1;
+    const bottom1 = y1 + h1;
+
+    const left2 = x2;
+    const right2 = x2 + w2;
+    const top2 = y2;
+    const bottom2 = y2 + h2;
+
+    return !(left1 > right2 || right1 < left2 || top1 > bottom2 || bottom1 < top2);
+}
+
+export function getDistance(x1: number, y1: number, x2: number, y2: number): number {
+    return Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
+}
