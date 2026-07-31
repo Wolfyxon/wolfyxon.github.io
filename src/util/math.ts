48a2f8906e1e0e86fe9d6f9de35aaacc2f09d225
Wolfyxon <wolfyxon@gmail.com>
2026-07-31 11:48:34 +0200

    Added angleTo()

--- src/util/math.ts
+++ src/util/math.ts
@@ -1,3 +1,7 @@
+export type Vector2 = {
+    x: number,
+    y: number
+};
 
 export function clamp(num: number, min: number, max: number): number {
     if(num < min) return min;
@@ -24,4 +28,11 @@ export function pad(number: number, zeros?: number): string {
 
 export function deg2rad(deg: number) {
     return deg * Math.PI / 180;
+}
+
+export function angleTo(x1: number, y1: number, x2: number, y2: number) {
+    const dx = x2 - x1;
+    const dy = y2 - y1;
+    
+    return Math.atan2(dy, dx);
 }
