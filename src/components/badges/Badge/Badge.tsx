3a76ebd618d46d15c0cc262a59e07e7395de3337
Wolfyxon <wolfyxon@gmail.com>
2026-08-19 23:59:09 +0200

    Pointer cursor for clickable badges

--- src/components/badges/Badge/Badge.tsx
+++ src/components/badges/Badge/Badge.tsx
@@ -8,7 +8,12 @@ export default function Badge(props: {
     onClick?: () => any | void
 } & ElmBase) {
     return (
-        <div className={classJoin("badge", props.className)} id={props.id} onClick={props.onClick}>
+        <div 
+            className={classJoin("badge", props.className)} 
+            id={props.id} 
+            onClick={props.onClick}
+            style={props.onClick ? {cursor: "pointer"} : undefined}
+        >
             <img className="badge-img" src={"/assets/img/icons/" + props.icon} alt={props.alt} />
             <div className="badge-label" translate={props.translate ?? "no"}>{props.alt}</div>
         </div>
